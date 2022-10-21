59bad706a5977e8de9c19d3972c5d1cb0e1f042f
ianrmcdonald <ianrmcdonald@gmail.com>
2022-10-21 13:02:35 -0700

    New commit

--- konrad.R
+++ konrad.R
@@ -1,2 +1,11 @@
 #https://www.r-bloggers.com/2021/03/simplifying-geospatial-features-in-r-with-sf-and-rmapshaper/
 
+library(ggplot2)
+library(magrittr)
+library(sf)
+
+
+mv <- st_read('data/mv.geojson') %>%
+     st_geometry() %>%
+     st_transform(crs = '+proj=aeqd +lat_0=53.6 +lon_0=12.7')
+# centered at input data for low distortion
